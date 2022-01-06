418a3fa3bf1b435deb688b2fcfdfce466e0cbb30
cgabriel <christian.gabriel@ift-informatik.de>
2022-01-06 15:38:45 +0100

    sync_bat

--- parser/tocsv.py
+++ parser/tocsv.py
@@ -8,8 +8,9 @@ from konto.base import config
 
 class Tocsv (object):
 
-    def __init__ (self,dir="."):
-        self.dir = dir
+    def __init__ (self,maxzeile=200,dir="."):
+        self.dir      = dir
+        self.maxzeile = maxzeile
     
 #*********************************************************************************
 
@@ -29,54 +30,93 @@ class Tocsv (object):
             m = re.search("^(.*)\.(.*)$",ktofile)
             if m:
                 fileroot = m.group(1)
-                if not m.group(2) in ("kto","html"):
+                if not m.group(2).lower() in ("kto","html"):
                     continue
             else:
                 continue
         
-            text        = open(ktofile[0]).read()
+            text        = open(ktofile).read()
             text1       = []
+            text2       = []
             einrueckung = {}
             
-            for zeile in text.split("\n"):
+            maxzeile1   = 0
             
-                m = re.search(r"^(\S+|\S+ +\S+) +([0123456789abcdef]+) +(\-?\d+\.\d\d) +",zeile)
+            for zeile in text.split("\n"):
+                            
+                m = re.search(r"^(\S+|\S+ +\S+) +([0123456789abcdef]+) +(\-?\d+\.\d\d) *",zeile)
                 if m:
-                    betrag = re.sub(m.group(3),"\.",",")
-                    text1.append(m.group(1) + ";;" + m.group(2) + ";;" + betrag + ";")
+                    betrag = re.sub(r"\.",".",m.group(3))
+                    text1.append('"' + m.group(1) + '";;"' + m.group(2) + '";;' + betrag + ";")
+                    text2.append( self.normalize_text(zeile) )
                     continue
 
-                m = re.search(r"^(\d\d\d\d)(\d\d)(\d\d) +(\-?\d+\.\d\d) +(\S+) +(\S+) +(\-?\d+\.\d\d) +(.*)$",zeile)
+                m = re.search(r"^\d\d(\d\d)(\d\d)(\d\d) +(\-?\d+\.\d\d) +(\S+) +(\S+) +(\-?\d+\.\d\d) +(.*)$",zeile)
                 if m:
-                    betrag1 = re.sub(m.group(2),"\.",",")
-                    betrag2 = re.sub(m.group(5),"\.",",")
-                    text1.append(m.group(1) + ";" + betrag1 + ";" + m.group(3) + ";" + m.group(4) + ";" + betrag2 + ";" + m.group(6) + ";")
+                    betrag1 = re.sub(r"\.",".",m.group(4))
+                    betrag2 = re.sub(r"\.",".",m.group(7))
+                    text1.append('"' + m.group(3) + "." + m.group(2) + "." + m.group(1) + '";' + betrag1 + ';"' + m.group(5) + '";"' + m.group(6) + '";' + betrag2 + ';"' + m.group(8) + '";')
+                    text2.append( self.normalize_text(zeile)[0:self.maxzeile] )
+                    maxzeile1 = min( max(maxzeile1,len(text2[-1])), self.maxzeile)
                     continue
                     
-                m = re.search(r"^ +(\-?\d+\.\d\d) +$",zeile)
+                m = re.search(r"^ +(\-?\d+\.\d\d) *$",zeile)
                 if m:
-                    betrag           = re.sub(m.group(1),"\.",",")
-                    ind              = betrag.index(",")
-                    einrueckung[ind] = 2
-                    text1.append(";;" + betrag)
+                    betrag           = re.sub(r"\.",".",m.group(1))
+                    text1.append(";" + betrag + ";")
+                    text2.append( self.normalize_text(zeile) )
                     continue
                     
-                m = re.search(r"^(\S+) +(\-?\d+\.\d\d) +$",zeile)
+                m = re.search(r"^(\S+) +(\-?\d+\.\d\d) *$",zeile)
                 if m:
-                    betrag           = re.sub(m.group(2),"\.",",")
-                    ind              = betrag.index(",")
+                    betrag           = re.sub(r"\.",".",m.group(2))
+                    ind              = zeile.index(".")
                     if not ind in einrueckung:
-                        einrueckung[ind] = len(einrueckung) + 1
-                    text1.append(m.group(1) + (";" * einrueckung[ind]) + betrag)
+                        einrueckung[ind] = len(einrueckung) + 2
+                    text1.append('"' + m.group(1) + '"' + (";" * einrueckung[ind]) + betrag + ";")
+                    text2.append( self.normalize_text(zeile) )
                     continue
                     
                 if "<PRE>" in zeile.upper() or "</PRE>" in zeile.upper():
                     continue
                     
-                text1.append(";")
+                text1.append('"' + zeile + '"')
+                text2.append(self.normalize_text(zeile)[0:self.maxzeile])
                 
-                open(self.dir + "/" + fileroot + ".csv","w").write("\n".join(text1)+"\n")
+            open(self.dir + "/" + fileroot + ".csv","w").write("\n".join(text1)+"\n")
+                
+            open("__tt.txt","w").write("\n".join(text2)+"\n")
+            print(maxzeile1)
+            os.system("a2ps -B -1 -l " + str(maxzeile1) + " -r -o __tt.ps __tt.txt")
+            os.system("ps2pdf __tt.ps " + fileroot + ".pdf")
+            os.unlink("__tt.txt")
+            os.unlink("__tt.ps")
             
+#************************************************************************************************************
+
+    def normalize_text (self,text):
+    
+        text = re.sub(r"ä",   "ae",text,99999999)
+        text = re.sub(r"ö",   "oe",text,99999999)
+        text = re.sub(r"ü",   "ue",text,99999999)
+        text = re.sub(r"Ä",   "Ae",text,99999999)
+        text = re.sub(r"Ö",   "Oe",text,99999999)
+        text = re.sub(r"Ü",   "Ue",text,99999999)
+        text = re.sub(r"ß",   "ss",text,99999999)
+        text = re.sub(r"a\"", "ae",text,99999999)
+        text = re.sub(r"o\"", "oe",text,99999999)
+        text = re.sub(r"u\"", "ue",text,99999999)
+        text = re.sub(r"A\"", "Ae",text,99999999)
+        text = re.sub(r"O\"", "Oe",text,99999999)
+        text = re.sub(r"U\"", "Ue",text,99999999)
+        text = re.sub(r"s\"", "ss",text,99999999)
+        text = re.sub(r"&",   "u", text,99999999)
+        text = re.sub(r"\t",  "  ",text,99999999)
+        text = re.sub(chr(13),"",  text,99999999)
+        
+        return(text)
+
+
 #******************************************************************************
 
 
